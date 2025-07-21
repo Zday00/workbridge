7361a4afd796bdf9819159c7f4ecd5f8575c9399
Zday00 <yasserzabeirou00@gmail.com>
2025-07-21 11:19:31 +0100

    feat: Add candidate and mission relationships to Application model

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -8,4 +8,14 @@ class Application extends Model
 {
     protected $fillable = ['candidate_id', 'mission_id', 'status', 'cover_letter'];
     protected $dates = ['applied_at'];
+
+    public function candidate()
+    {
+        return $this->belongsTo(Candidate::class);
+    }
+
+    public function mission()
+    {
+        return $this->belongsTo(Mission::class);
+    }
 }
