1371243b008c3ca4cbff0e50cd9c66a34f6b0939
Zday00 <yasserzabeirou00@gmail.com>
2025-07-22 22:16:37 +0100

    Feat: Add authentication routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get(uri: '/', action: function () {
+    return view(view: 'welcome');
 });
+Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
+
+Route::post('/register', [AuthController::class], 'register')->name('register');
+Route::post('/login', [AuthController::class], 'login')->name('login');
