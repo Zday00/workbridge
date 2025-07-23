1bd402397a4271568570ccdc734390dd91b8c91e
Zday00 <yasserzabeirou00@gmail.com>
2025-07-23 08:25:15 +0100

    Separate auth routes for applicants and recruiters

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,37 @@ use Illuminate\Support\Facades\Route;
 Route::get(uri: '/', action: function () {
     return view(view: 'welcome');
 });
-Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
-Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
 
-Route::post('/register', [AuthController::class], 'register')->name('register');
+// Séparation des routes en fonctions des rôles 
+
+// Inscription et connexion pour postulant
+Route::get('/register/applicant', [AuthController::class, 'showApplicantRegister'])->name('show.applicant.register');
+Route::get('/login/applicant', [AuthController::class, 'showApplicantLogin'])->name('show.applicant.login');
+
+// Inscription et connexion pour recruteur
+Route::get('/register/recruiter', [AuthController::class, 'showRecruiterRegister'])->name('show.recruiter.register');
+Route::get('/login/recruiter', [AuthController::class, 'showRecruiterLogin'])->name('show.recruiter.login');
+
+// Traitement inscription du postulant
+Route::post('/register/applicant', [AuthController::class, 'applicantRegister'])->name('applicant.register');
+
+// Traitement inscription du recruteur
+Route::post('/register/recruiter', [AuthController::class, 'recruiterRegister'])->name('recruiter.register');
+
+// Traitement connexion du postulant
+Route::post('/login/applicant', [AuthController::class, 'applicantLogin'])->name('applicant.login');
+
+// Traitement connexion du recruteur
+Route::post('/login/recruiter', [AuthController::class, 'recruiterLogin'])->name('recruiter.login');
+
+
+
+
+
+
+
+
+
+
+
 Route::post('/login', [AuthController::class], 'login')->name('login');
