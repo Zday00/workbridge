cf4d2a0627c06ed1b96590c3ac143f0275ff7b10
Zday00 <yasserzabeirou00@gmail.com>
2025-07-24 23:20:07 +0100

    fix AuthController new function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -74,6 +74,51 @@ class AuthController extends Controller
     ]);
 }
 
+    public function recruiterRegister(Request $request)
+{
+    $messages = [
+        'first_name.required' => 'Le prénom est obligatoire',
+        'last_name.required' => 'Le nom est obligatoire',
+        'email.unique' => 'Cet email est déjà utilisé',
+        'email.email' => 'Format email invalide',
+        'company_name.required' => 'Le nom de l\'entreprise est obligatoire',
+        'industry.required' => 'Le nom de l\'industrie est obligatoire',
+        'industry.min' => 'Le nom de l\'industrie doit être au moins 3 caractères ',
+        'industry.max' => 'Le nom de l\'industrie ne doit pas dépasser 20 caractères ',
+        'password.confirmed' => 'Les mots de passe ne correspondent pas',
+        'password.regex' => 'Le mot de passe doit contenir au moins une majuscule, une minuscule et un chiffre',
+    ];
+
+    $validatedData = $request->validate([
+        'first_name' => 'required|string|min:2|max:25',
+        'last_name' => 'required|string|min:2|max:25',
+        'email' => 'required|string|email|unique:users,email',
+        'company_name' => 'required|string|min:3|max:20',
+        'website' => 'nullable|url|max:255',
+        'industry' => 'required|string|min:3|max:40',
+        'password' => 'required|string|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/|confirmed',
+        'password_confirmation' => 'required'
+    ], $messages);
+
+    $user = User::create([
+        'first_name' => $validatedData['first_name'],
+        'last_name' => $validatedData['last_name'],
+        'email' => $validatedData['email'],
+        'password' => Hash::make($validatedData['password']),
+        'role' => 'recruteur',
+    ]);
+
+    $user->recruiter()->create([
+        'company_name' => $validatedData['company_name'],
+        'industry' => $validatedData['industry'],
+        'website' => $validatedData['website'],
+    ]);
+
+    return view('welcome', [
+        'user' => $user,
+        'message' => 'Votre compte recruteur a été créé avec succès !'
+    ]);
+} 
 
 
 
