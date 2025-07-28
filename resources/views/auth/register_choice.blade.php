14b93a4efc466ecd133a81bcb0c080c14127e9a3
Zday00 <yasserzabeirou00@gmail.com>
2025-07-28 16:41:20 +0100

    modify content and delete layout files

--- resources/views/auth/register_choice.blade.php
+++ resources/views/auth/register_choice.blade.php
@@ -1,8 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Essai</title>
+</head>
+
 <body>
-    @include('layouts.header')
-
-
-
     <main>
 
         <h2>Choisissez votre rôle</h2>
@@ -16,9 +22,6 @@
         </a>
 
     </main>
-
-
-
-
-    @include('layouts.footer')
 </body>
+
+</html>
