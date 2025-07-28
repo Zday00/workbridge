d145e5b380c927c8065bda5ad33f96fa354e5a8f
Zday00 <yasserzabeirou00@gmail.com>
2025-07-28 16:42:07 +0100

    add a route(test-mail)

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,14 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
-
+ use Illuminate\Support\Facades\Mail;
 Route::get(uri: '/', action: function () {
     return view(view: 'layouts/homepage');
 });
 
 // Séparation des routes en fonctions des rôles 
 
-// Inscription et connexion
+// Inscription
 Route::get('/register', [AuthController::class, 'showRegisterChoice'])->name('show.register.choice');
 
 
@@ -24,7 +24,7 @@ Route::get('/register/recruiter', [AuthController::class, 'showRegisterRecruiter
 Route::post('/register/applicant', [AuthController::class, 'applicantRegister'])->name('register.applicant');
 Route::post('/register/recruiter',[AuthController::class, 'recruiterRegister'])->name('register.recruiter');
 
-//Formulaire du code OTP
+//Formulaire du code OTP |  afficher le formulaire dans lequel l’utilisateur va saisir le code OTP
 Route::get('/verify-email', [AuthController::class, 'showVerifyForm'])->name('verification.form');
 
 //Traitement de la vérification OTP
@@ -33,5 +33,17 @@ Route::post('/verify-email', [AuthController::class, 'VerifyOtp'])->name('verifi
 //Renvoi de code OTP en cas d'echec
 Route::get('/resend-otp', [AuthController::class, 'ResendOtp'])->name('verification.resend');
 
-// Affichage du formulaire OTP
-Route::get('/verify-otp', [AuthController::class, 'showOtpForm' ])->name('verify.otp.form');
+//route temp
+use App\Mail\VerificationCodeMail;
+
+Route::get('/test-email', function () {
+    try {
+        $reponse = Mail::to('ggol71884@gmail.com')->send(new VerificationCodeMail(123456));
+        Log::info("Reponse mail",[$reponse]);
+        return 'Email envoyé avec succès !';
+    } catch (\Exception $e) {
+        return 'Erreur : ' . $e->getMessage();
+    }
+});
+
+
