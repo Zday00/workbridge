b71eea5ce451ce0f14bc3b898ee78d05dd737cf6
Zday00 <yasserzabeirou00@gmail.com>
2025-07-30 13:05:50 +0100

    Added Authenticatable trait to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,11 +1,10 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-use Illuminate\Database\Eloquent\Model;
-
-class User extends Model
+class User extends Authenticatable
 {
     protected $fillable = ['first_name', 'last_name', 'email', 'password', 'role'];
 
